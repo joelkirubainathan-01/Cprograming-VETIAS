584356b6f2991b5fe2cc082a0e3a7fa6dd4c7bc2
joelkirubainathan-01 <joelkirubainathansaminathan23csa@vetias.ac.in>
2025-05-17 08:08:06 +0000

    renamed:    Day 6/sam.c -> Day 6/day6-1.printarray_program.c 	new file:   Day 6/day6-2.remspace_program.c 	new file:   day6-3.dynamicarray_program.c

--- Day 6/day6-1.printarray_program.c
+++ Day 6/day6-1.printarray_program.c
@@ -4,14 +4,12 @@ int main() {
     int n;
     scanf("%d", &n);
 
-    int arr[n];
-
+    int a[n];
     for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
+        scanf("%d", &a[i]);
     }
-
-    for (int i = n - 1; i >= 0; i--) {
-        printf("%d ", arr[i]);
+    for (int i = 0; i < n; i++) {
+        printf("%d ", a[i]);
     }
 
     return 0;
